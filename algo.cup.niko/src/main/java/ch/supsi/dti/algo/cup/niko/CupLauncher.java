751b35d6a666dd9a0eb56b22d43dda0d572dd0dc
Niko Storni <niko@storni.info>
2016-04-22 09:23:38 +0200

    fl1577

--- algo.cup.niko/src/main/java/ch/supsi/dti/algo/cup/niko/CupLauncher.java
+++ algo.cup.niko/src/main/java/ch/supsi/dti/algo/cup/niko/CupLauncher.java
@@ -25,11 +25,11 @@ public class CupLauncher
 		// sligthly harder
 		// problems.put("lin318", TSPParser.parse("lin318.tsp"));
 		// problems.put("d198", TSPParser.parse("d198.tsp"));
-		// problems.put("pcb442", TSPParser.parse("pcb442.tsp"));
+		problems.put("pcb442", TSPParser.parse("pcb442.tsp"));
 		// problems.put("pr439", TSPParser.parse("pr439.tsp"));
 
 		// hard as fuck
-		problems.put("rat783", TSPParser.parse("rat783.tsp"));
+		// problems.put("rat783", TSPParser.parse("rat783.tsp"));
 		// problems.put("fl1577", TSPParser.parse("fl1577.tsp"));
 		// problems.put("u1060", TSPParser.parse("u1060.tsp"));
 	}

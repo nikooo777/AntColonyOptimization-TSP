795557920674f31c4fcb68bc8fd2a59a6959ead2
Niko Storni <niko@storni.info>
2016-04-23 12:06:49 +0200

    Seed for pcb442: 1461381819514 with a performance of: 0.0413564929693962%

--- algo.cup.niko/src/main/java/ch/supsi/dti/algo/cup/niko/CupLauncher.java
+++ algo.cup.niko/src/main/java/ch/supsi/dti/algo/cup/niko/CupLauncher.java
@@ -23,8 +23,8 @@ public class CupLauncher {
 		// sligthly harder
 		// problems.put("lin318", TSPParser.parse("lin318.tsp"));
 		// problems.put("d198", TSPParser.parse("d198.tsp"));
-		// problems.put("pcb442", TSPParser.parse("pcb442.tsp"));
-		// problems.put("pr439", TSPParser.parse("pr439.tsp"));
+		problems.put("pcb442", TSPParser.parse("pcb442.tsp"));
+		 //problems.put("pr439", TSPParser.parse("pr439.tsp"));
 
 		// hard as fuck
 		// problems.put("rat783", TSPParser.parse("rat783.tsp"));
@@ -45,7 +45,7 @@ public class CupLauncher {
 		for (final String s : problems.keySet()) {
 			System.out.println("---->" + s);
 			int localbest = Integer.MAX_VALUE;
-			for (int i = 0; i < 15; i++) {
+			for (int i = 0; i < 200; i++) {
 				overallstarttime = System.currentTimeMillis() + parseTime;
 				long seed = System.currentTimeMillis();
 				System.out.println("seed: " + seed);

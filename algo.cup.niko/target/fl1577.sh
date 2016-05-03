0b653ee31977e1d18a282f74397268e5236bce97
Niko Storni <niko@storni.info>
2016-05-03 20:13:20 +0200

    almost didn't see this one

--- algo.cup.niko/target/fl1577.sh
+++ algo.cup.niko/target/fl1577.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar algo.cup.niko-0.0.1-SNAPSHOT.jar fl1577 true true 2 0.1 0.1 0.995 0 15 1462180675427
+java -jar algo.cup.niko-0.0.1-SNAPSHOT.jar fl1577 true true 0.1 2 0.1 0.995 0 15 179000
